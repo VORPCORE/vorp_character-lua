bd13a99b6a5d0c1d964dafbd087750659056fb2e
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-03-27 04:06:02 +0100

    update some fixes

--- server/server.lua
+++ server/server.lua
@@ -9,70 +9,10 @@ TriggerEvent("getCore", function(core)
 	end)
 end)
 
-
-AddEventHandler("vorp_SpawnUniqueCharacter", function(source)
-	local User = VorpCore.getUser(source)
-	local Characters = User.getUserCharacters
-	local userCharacters = {}
-	for _, characters in pairs(Characters) do
-		local userChars = {
-			charIdentifier = characters.charIdentifier,
-			money = characters.money,
-			gold = characters.gold,
-			firstname = characters.firstname,
-			lastname = characters.lastname,
-			skin = json.decode(characters.skin),
-			components = json.decode(characters.comps),
-			coords = json.decode(characters.coords),
-			isDead = characters.isdead
-		}
-		userCharacters[#userCharacters + 1] = userChars
-	end
-	TriggerClientEvent("vorpcharacter:spawnUniqueCharacter", source, userCharacters)
-end)
-
-
 RegisterServerEvent("vorp_CreateNewCharacter", function(source)
 	TriggerClientEvent("vorpcharacter:startCharacterCreator", source)
 end)
 
-
-RegisterServerEvent("vorp_GoToSelectionMenu")
-AddEventHandler("vorp_GoToSelectionMenu", function(source)
-	local _source = source
-	if _source == nil then
-		print("Source is nil")
-		return
-	end
-
-	local characters = VorpCore.getUser(_source).getUserCharacters
-	local auxcharacter
-	local UserCharacters = {}
-
-	for _, character in pairs(characters) do
-		auxcharacter = {
-			charIdentifier = character.charIdentifier,
-			money = character.money,
-			gold = character.gold,
-			firstname = character.firstname,
-			lastname = character.lastname,
-			skin = json.decode(character.skin),
-			components = json.decode(character.comps),
-			coords = json.decode(character.coords),
-			isDead = character.isdead
-		}
-		UserCharacters[#UserCharacters + 1] = auxcharacter
-	end
-
-	TriggerClientEvent("vorpcharacter:selectCharacter", _source, UserCharacters, MaxCharacters)
-end)
-
-
-
-VorpCore.addRpcCallback("vorp_characters:Client:GetComponents", function(source, cb, args)
-	local character = VorpCore.getUser(source).getUserCharacters
-	cb(json.decode(character.comps))
-end)
 
 RegisterServerEvent("vorpcharacter:saveCharacter")
 AddEventHandler("vorpcharacter:saveCharacter", function(skin, clothes, firstname, lastname)
@@ -88,7 +28,7 @@ end)
 RegisterServerEvent("vorpcharacter:deleteCharacter")
 AddEventHandler("vorpcharacter:deleteCharacter", function(charid)
 	local _source = source
-	print("Deleting character " .. charid)
+
 	local User = VorpCore.getUser(_source)
 	User.removeCharacter(charid)
 end)
@@ -121,4 +61,94 @@ RegisterNetEvent("vorpcharacter:setPlayerCompChange", function(skinValues, comps
 			User.updateSkin(json.encode(skinValues))
 		end
 	end
+end)
+
+local function Checkmissingkeys(information, key)
+	local switch = false
+	if key == "skin" then
+		for k, v in pairs(PlayerSkin) do
+			if information[k] == nil then
+				switch = true
+				information[k] = v
+			end
+		end
+		return information, switch
+	end
+	if key == "comps" then
+		for k, v in pairs(PlayerClothing) do
+			if information[k] == nil then
+				information[k] = v
+			end
+		end
+		return information, switch
+	end
+end
+
+AddEventHandler("vorp_SpawnUniqueCharacter", function(source)
+	local User = VorpCore.getUser(source)
+	local Characters = User.getUserCharacters
+	local userCharacters = {}
+	for _, characters in pairs(Characters) do
+		local skin, switch1 = Checkmissingkeys(json.decode(characters.skin), "skin")
+		local comps, switch2 = Checkmissingkeys(json.decode(characters.comps), "comps")
+		if switch1 then
+			characters.updateSkin((json.encode(skin)))
+		end
+		if switch2 then
+			characters.updateComps(json.encode(comps))
+		end
+		local userChars = {
+			charIdentifier = characters.charIdentifier,
+			money = characters.money,
+			gold = characters.gold,
+			firstname = characters.firstname,
+			lastname = characters.lastname,
+			skin = skin,
+			components = comps,
+			coords = json.decode(characters.coords),
+			isDead = characters.isdead
+		}
+		userCharacters[#userCharacters + 1] = userChars
+	end
+	TriggerClientEvent("vorpcharacter:spawnUniqueCharacter", source, userCharacters)
+end)
+
+RegisterServerEvent("vorp_GoToSelectionMenu")
+AddEventHandler("vorp_GoToSelectionMenu", function(source)
+	local _source = source
+	if _source == nil then
+		print("Source is nil")
+		return
+	end
+
+	local characters = VorpCore.getUser(_source).getUserCharacters
+	local auxcharacter
+	local UserCharacters = {}
+
+	for _, character in pairs(characters) do
+		local skin, switch1 = Checkmissingkeys(json.decode(character.skin), "skin")
+		local comps, switch2 = Checkmissingkeys(json.decode(character.comps), "comps")
+
+		if switch1 then
+			character.updateSkin((json.encode(skin)))
+		end
+		if switch2 then
+			character.updateComps(json.encode(comps))
+		end
+
+		auxcharacter = {
+			charIdentifier = character.charIdentifier,
+			money = character.money,
+			gold = character.gold,
+			firstname = character.firstname,
+			lastname = character.lastname,
+			skin = skin,
+			components = comps,
+			coords = json.decode(character.coords),
+			isDead = character.isdead
+		}
+		UserCharacters[#UserCharacters + 1] = auxcharacter
+	end
+
+	TriggerClientEvent("vorpcharacter:selectCharacter", _source, UserCharacters, MaxCharacters)
 end)
