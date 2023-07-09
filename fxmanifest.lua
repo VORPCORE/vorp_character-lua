1e1130dd50ae880c5a9fd7e86e383808586cb6fe
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-07-09 18:38:23 +0100

    feat: add new character selection

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -28,8 +28,7 @@ files {
 
 --========= VERSION =============--
 
-
-version '1.3'
+version '1.4'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_character-lua'
