b7a94eb588525b0e29dd8a437dc7e66ac79bc892
KadDarem <cressent.h@orange.fr>
2024-01-30 06:18:49 +0100

    resolve conflict

--- client/menu.lua
+++ client/menu.lua
@@ -846,7 +846,7 @@ function OpenComponentMenu(table, category, value, Outfits)
                 _G[data.trigger](table, value, Outfits)
             end
 
-            if data.current.action == "swapPalette" and data.current.comp ~= -1 then
+            if data.current.action == "swapPalette" and data.current.comp ~= -1 and TagData and next(TagData) then
                 IsPedReadyToRender()
                 local comp = data.current.comp
                 local palette = ConfigShops.palettes[data.current.value]
@@ -861,7 +861,7 @@ function OpenComponentMenu(table, category, value, Outfits)
                 return
             end
 
-            if data.current.action == "tint0" and data.current.comp ~= -1 then
+            if data.current.action == "tint0" and data.current.comp ~= -1 and TagData and next(TagData) then
                 IsPedReadyToRender()
                 local comp = data.current.comp
                 local tint0, tint1, tint2 = data.current.value, PlayerTrackingData[category][comp].tint1,
@@ -878,7 +878,7 @@ function OpenComponentMenu(table, category, value, Outfits)
                 return
             end
 
-            if data.current.action == "tint1" and data.current.comp ~= -1 then
+            if data.current.action == "tint1" and data.current.comp ~= -1 and TagData and next(TagData) then
                 IsPedReadyToRender()
                 local comp = data.current.comp
                 local tint0, tint1, tint2 = PlayerTrackingData[category][comp].tint0, data.current.value,
@@ -895,7 +895,7 @@ function OpenComponentMenu(table, category, value, Outfits)
                 return
             end
 
-            if data.current.action == "tint2" and data.current.comp ~= -1 then
+            if data.current.action == "tint2" and data.current.comp ~= -1 and TagData and next(TagData) then
                 IsPedReadyToRender()
                 local comp = data.current.comp
                 local tint0, tint1, tint2 = PlayerTrackingData[category][comp].tint0,
