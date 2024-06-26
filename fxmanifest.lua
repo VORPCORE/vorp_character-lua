701f06022ec59905add50812e9bad56e58269460
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-06-26 15:29:53 +0100

    chore(client): remove notify.js file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,8 @@ game 'rdr3'
 lua54 'yes'
 
 -- Initial Convertion from C# to lua by grumpypoo
-author 'VORP refactored by @outsider'
+name 'vorp character'
+author 'VORP @outsider'
 description 'A Character creator with also shops built in for vorpcore framework'
 repository 'https://github.com/VORPCORE/vorp_character-lua'
 
@@ -20,7 +21,6 @@ client_scripts {
 	'shared/clothing.lua',
 	'shared/hairs.lua',
 	'client/*.lua',
-	'client/notify.js',
 }
 
 server_scripts {
@@ -29,13 +29,11 @@ server_scripts {
 }
 
 files {
-	--'ui/*',
 	'images/*png',
 	'images/CreatorImages/*png',
 	'clothingfemale/*png',
 }
 
---========= VERSION =============--
 
 version '1.6'
 vorp_checker 'yes'
