4aef4041ce6a48b5e85646bc3b4f4da3f836751b
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-09-11 03:57:00 +0100

    chore(client): warn spawn locations not sufficient for amount of characters

--- client/client.lua
+++ client/client.lua
@@ -280,6 +280,7 @@ function StartSwapCharacters()
 	for key, value in pairs(myChars) do
 		LoadPlayer(value.skin.sex)
 		local data = Config.SpawnPosition[random].positions[key]
+		if not data then return error("your config spawn locations doesnt have enough spawn locations you need to add: " .. #myChars .. "Spawn locations") end
 		data.PedHandler = CreatePed(joaat(value.skin.sex), data.spawn.x, data.spawn.y, data.spawn.z, data.spawn.w, false, false, false, false)
 		repeat Wait(0) until DoesEntityExist(data.PedHandler)
 		LoadCharacterSelect(data.PedHandler, value.skin, value.components)
